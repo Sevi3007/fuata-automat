37bfae6a2a6b0efcefd8647ef6c79c5816b62fe4
Sevi3007 <severin.planer@bulme.at>
2025-11-20 17:37:35 +0100

    raufladen

--- Platformio_Severin_Planer/arduino-simon-game-main/src/main.cpp
+++ Platformio_Severin_Planer/arduino-simon-game-main/src/main.cpp
@@ -1,10 +1,20 @@
 
 #include <Arduino.h>
-
+int counter = 0; 
 void setup() {
   Serial.begin(9600);
 }
 void loop() {
-  Serial.println("Hello, Arduino!");
-  delay(1000);
+  
+if counter % 3 == 0 {
+    Serial.println("Fizz");
+  } else if counter % 5 == 0 {
+    Serial.println("Buzz");
+  } else {
+    Serial.println(counter);
+  }
+  Serial.print("Counter: ");
+  Serial.println(counter);
+  counter++;
+  delay(1000); // wait for a second
 }
